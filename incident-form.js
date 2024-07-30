595c3efeae00f983ca81461a0b8cb958cccf72f6
Durojaiye Isaiah <isaiahdurojaiye9@gmail.com>
2024-07-30 14:20:35 +0100

    responsive design of the form page

--- incident-form.js
+++ incident-form.js
@@ -15,4 +15,14 @@ for (option of options) {
     list.classList.toggle("hide")
     arrowIcon.classList.toggle("rotate")
   }
+  $("input:checkbox").on('click', function() {
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } else {
+      $box.prop("checked", false);
+    }
+  });
 }
