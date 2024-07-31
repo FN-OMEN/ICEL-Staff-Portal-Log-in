6722ffc7978a45821c6849bad7b5aec1e2d320b2
Durojaiye Isaiah <isaiahdurojaiye9@gmail.com>
2024-07-31 09:27:18 +0100

    Corrected the response of the button on mobile versions

--- travel.js
+++ travel.js
@@ -9,17 +9,15 @@ function hideForm() {
 }
 
 document.getElementById('travelForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
+  event.preventDefault(); 
   const destination = document.getElementById('destination').value;
   const checkin = document.getElementById('checkin').value;
   const checkout = document.getElementById('checkout').value;
   const hotel = document.getElementById('hotel').value;
 
-  // For demonstration purposes, we'll just log the form data to the console
   console.log('Destination:', destination);
   console.log('Check-in Date:', checkin);
   console.log('Check-out Date:', checkout);
   console.log('Hotel:', hotel);
 
-  // You can add your form submission logic here, such as sending the data to a server
 });
